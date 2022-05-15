2d667c866b4f95ec968d3985dfab16e1d2be189b
Asmaxx <72007833+Asmaxx@users.noreply.github.com>
2022-05-15 21:27:31 +0200

    Create CHI_SQUARED_AND_PCA.js

---
+++ CHI_SQUARED_AND_PCA.js
@@ -0,0 +1,17 @@
+import chiSquaredTest from "chi-squared-test";
+
+const expected = [5.1,4.9,4.7,4.6,5.0,5.4,4.6,5.0,4.4,4.9,5.4,4.8,4.8,4.3,5.8,5.7,5.4,5.1,5.7,5.1,5.4,5.1,4.6,5.1,4.8,5.0,5.0,5.2,5.2,4.7,4.8,5.4,5.2,5.5,4.9,5.0,5.5,4.9,4.4,5.1,5.0,4.5,4.4,5.0,5.1,4.8,5.1,4.6,5.3,5.0];
+let observed = [7.0,6.4,6.9,5.5,6.5,5.7,6.3,4.9,6.6,5.2,5.0,5.9,6.0,6.1,5.6,6.7,5.6,5.8,6.2,5.6,5.9,6.1,6.3,6.1,6.4,6.6,6.8,6.7,6.0,5.7,5.5,5.5,5.8,6.0,5.4,6.0,6.7,6.3,5.6,5.5,5.5,6.1,5.8,5.0,5.6,5.7,5.7,6.2,5.1,5.7];
+const reduction = 1;
+
+let probability = chiSquaredTest(observed, expected, reduction);
+
+probability = chiSquaredTest(observed, expected, reduction);
+var data = [[5.1,4.9,4.7,4.6,5.0,5.4,4.6,5.0,4.4,4.9,5.4,4.8,4.8,4.3,5.8,5.7,5.4,5.1,5.7,5.1,5.4,5.1,4.6,5.1,4.8,5.0,5.0,5.2,5.2,4.7,4.8,5.4,5.2,5.5,4.9,5.0,5.5,4.9,4.4,5.1,5.0,4.5,4.4,5.0,5.1,4.8,5.1,4.6,5.3,5.0],
+    [7.0,6.4,6.9,5.5,6.5,5.7,6.3,4.9,6.6,5.2,5.0,5.9,6.0,6.1,5.6,6.7,5.6,5.8,6.2,5.6,5.9,6.1,6.3,6.1,6.4,6.6,6.8,6.7,6.0,5.7,5.5,5.5,5.8,6.0,5.4,6.0,6.7,6.3,5.6,5.5,5.5,6.1,5.8,5.0,5.6,5.7,5.7,6.2,5.1,5.7]];
+var vectors = PCA.getEigenVectors(data);
+var first = PCA.computePercentageExplained(vectors,vectors[0])
+var topTwo = PCA.computePercentageExplained(vectors,vectors[0],vectors[1])
+var adData = PCA.computeAdjustedData(data,vectors[0])
+var compressed = adData.formattedAdjustedData;
+var uncompressed = PCA.computeOriginalData(compressed,adData.selectedVectors,adData.avgData);
